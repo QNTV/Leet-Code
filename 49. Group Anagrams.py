02f255281b6e408fdd3ee0ccfa604bd06eb4af5d
Quang Nguyen <quangn8888@gmail.com>
2023-02-17 15:11:00 -0800

    update

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -30,6 +30,6 @@ class Solution:
         for s in strs:
             count = [0] * 26
             for c in s:
-                count[ord(c) - ord("a"0)] += 1
+                count[ord(c) - ord("a")] += 1
             ans[tuple(count)].append(s)
         return ans.values()
