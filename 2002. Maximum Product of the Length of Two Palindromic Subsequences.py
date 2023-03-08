71df0752dfe00080020c3183fdc807c78dcdefd7
Quang Nguyen <quangn8888@gmail.com>
2023-03-08 09:24:28 -0800

    update

--- 2002. Maximum Product of the Length of Two Palindromic Subsequences.py
+++ 2002. Maximum Product of the Length of Two Palindromic Subsequences.py
@@ -28,7 +28,7 @@
 # Constraints:
 #     2 <= s.length <= 12
 #     s consists of lowercase English letters only.
-$$TimeoutError
+
 class Solution:
     def maxProduct(self, s):
         N, pali = len(s), {} # bitmask : length
@@ -47,6 +47,37 @@ class Solution:
                 if m1 & m2 ==0: # disjoint
                     res = max(res, pali[m1] * pali[m2])
         return res
+    
+    
+class Solution:
+    def maxProduct(self, s):
+        n = len(s)
+        
+        first, last = [0]*(1<<n), [0]*(1<<n)
+        
+        for i in range(n):
+            for j in range(1<<i, 1<<(i+1)):
+                first[j] = i
+
+        for i in range(n):
+            for j in range(1<<i, 1<<n, 1<<(i+1)):
+                last[j] = i
+        
+        @lru_cache(None)
+        def dp(m):
+            if m & (m-1) == 0: return m != 0
+            l, f = last[m], first[m]
+            lb, fb = 1<<l, 1<<f
+            return max(dp(m-lb), dp(m-fb), dp(m-lb-fb) + (s[l] == s[f]) * 2)
+       
+        ans = 0
+        for m in range(1, 1<<n):
+            ans = max(ans, dp(m)*dp((1<<n) - 1 - m))
+            
+        return ans
+
+
+        
 solution = Solution()
 s = "accbcaxxcxx"
 print(solution.maxProduct(s))
