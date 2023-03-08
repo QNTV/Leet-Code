d1d19c1ce972b3a1894c36449cca03fc3f4b9663
Quang Nguyen <quangn8888@gmail.com>
2023-03-08 09:29:16 -0800

    update

--- 28. Find the Index of the First Occurrence in a String.py
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -52,4 +52,75 @@ class Solution:
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
-print(__file__.find)
+
+
+# Boyer-Moore algorithm 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+        
+        n = len(haystack)
+        m = len(needle)
+        
+        # create bad match table
+        bm_table = {}
+        for i in range(m-1):
+            bm_table[needle[i]] = m-1-i
+        
+        # search for the needle in the haystack
+        i = m-1
+        while i < n:
+            k = i
+            j = m-1
+            while j >= 0 and haystack[k] == needle[j]:
+                k -= 1
+                j -= 1
+            if j == -1:
+                return k+1
+            if haystack[i] in bm_table:
+                i += bm_table[haystack[i]]
+            else:
+                i += m
+        
+        return -1
+
+
+
+# Rabin-Karp algorithm
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+        
+        n = len(haystack)
+        m = len(needle)
+        p = 31
+        p_pow = [1]
+        for i in range(m-1):
+            p_pow.append(p_pow[-1] * p)
+        
+        # compute hashes for needle and first m characters of haystack
+        needle_hash = 0
+        for i in range(m):
+            needle_hash += ord(needle[i]) * p_pow[m-1-i]
+        haystack_hash = 0
+        for i in range(m):
+            haystack_hash += ord(haystack[i]) * p_pow[m-1-i]
+        if needle_hash == haystack_hash and haystack[:m] == needle:
+            return 0
+        
+        # compute rolling hash for haystack and check for needle match
+        for i in range(m, n):
+            haystack_hash -= ord(haystack[i-m]) * p_pow[m-1]
+            haystack_hash = haystack_hash * p + ord(haystack[i])
+            if haystack_hash == needle_hash and haystack[i-m+1:i+1] == needle:
+                return i-m+1
+        
+        return -1
