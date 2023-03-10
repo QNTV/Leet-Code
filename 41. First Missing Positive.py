31c53143dbb79bd97aa8cf62719037ada794c196
Quang Nguyen <quangn8888@gmail.com>
2023-03-09 17:00:48 -0800

    update

--- 41. First Missing Positive.py
+++ 41. First Missing Positive.py
@@ -21,5 +21,45 @@
 #     -231 <= nums[i] <= 231 - 1
 
 class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        
+    def firstMissingPositive(self, A) -> int:
+        for i in range(len(A)):
+            if A[i] < 0:
+                A[i] = 0
+                
+        for i in range(len(A)):
+            val = abs(A[i])
+            if 1 <= val <= len(A):
+                if A[val -1] > 0:
+                    A[val -1] *= -1
+                elif A[val - 1] == 0:
+                    A[val - 1] = -1 * (len(A) + 1)
+                    
+        for i in range(1, len(A) +1):
+            if A[i - 1] == 0:
+                return i
+        return len(A) + 1
+
+
+class Sol:
+  def firstMissingPositive(self, nums) -> int:
+        n = len(nums)
+		
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+        for i in range(n):
+            if abs(nums[i]) > n:
+                continue
+            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1])
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+        return n + 1
+
+
+
+Sol = Sol()
+Solution = Solution()
+A = [7,8,9,11,12]
+
+print(Sol.firstMissingPositive(A))
