f5d73403e234fcdcfdc1ef24ecf1277221480d93
Quang Nguyen <quangn8888@gmail.com>
2023-03-22 15:42:49 -0700

    update

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -5,17 +5,14 @@
 # (i.e., "ace" is a subsequence of "abcde" while "aec" is not). 
 
 # Example 1:
-
 # Input: s = "abc", t = "ahbgdc"
 # Output: true
 
 # Example 2:
-
 # Input: s = "axc", t = "ahbgdc"
 # Output: false 
 
 # Constraints:
-
 #     0 <= s.length <= 100
 #     0 <= t.length <= 104
 #     s and t consist only of lowercase English letters.
