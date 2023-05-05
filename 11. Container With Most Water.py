9805284eeb152f0024c7e760206328d364c28aee
Quang Nguyen <quangn8888@gmail.com>
2023-05-05 16:33:13 -0700

    update

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -23,4 +23,21 @@
 
 class Solution:
     def maxArea(self, height):
-        
+        l, r = 0, len(height) - 1
+        res = 0
+        h = max(height)
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            
+            if (r-l) * h <= res:
+                break 
+        return res 
+    
+sol = Solution()
+
+print(sol.maxArea([1,8,6,2,5,4,8,3,7]))
